a66f82c70d3eddc10caaa70fe909533e463100d8
pamykcharyyeva <pamyk.ch@gmail.com>
2020-10-12 13:01:26 -0500

    alg questions and answer

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-helllo world 
+const clamp = [2 ** 31 * -1, 2 ** 31 - 1]
+
+var reverse = function(x) {
+    const invert = Math.abs(x).toString().split('').reverse().join('')
+    const polar = x < 0 ? -1 : 1;
+    const t = Number(invert) * polar
+    
+    if (t < clamp[0] || t > clamp[1]) return 0
+    
+    return t
+};
